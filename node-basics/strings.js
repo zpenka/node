80cbd057249ba76727e73ce91fc4c7ea23eb25d9
zpenka <zpenka@gmail.com>
2015-09-12 15:39:57 -0400

    Adds strings files

---
+++ node-basics/strings.js
@@ -0,0 +1,5 @@
+var firstName = 'Zack';
+var lastName = 'Penka';
+var fullName = firstName + ' ' + lastName;
+
+console.log(fullName + ' is your name and it is ' + (fullName.length - 1) + ' characters long.');
