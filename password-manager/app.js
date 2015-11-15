0f422343e9f870ecf5bc2ec04f7cfeb8293fa281
zpenka <zpenka@gmail.com>
2015-11-14 21:13:43 -0500

    using node-persist

--- password-manager/app.js
+++ password-manager/app.js
@@ -1,1 +1,20 @@
 console.log("Starting password manager");
+
+var storage = require('node-persist');
+storage.initSync();
+
+storage.setItemSync('accounts', [{
+  username: 'Zack',
+  balance: 0
+}]);
+
+var accounts = storage.getItemSync('accounts');
+
+accounts.push({
+  username: "Jessica",
+  balance: 75
+});
+
+storage.setItemSync('accounts', accounts);
+
+console.log(accounts);
